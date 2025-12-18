eee15aeb4f550c28b4fb89847dabc13e436b1ba5
Kayvon devonte williams 🥷🥷🥷 <145631119+VelarApproximant@users.noreply.github.com>
2025-12-18 02:01:18 -0800

    Added movement on X axis and jumping..
    Prototype with no deco

---
+++ player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: float = 200
+@export var gravity: float = speed * 2
+@export var jump_speed: float = -speed * 2
+
+func _physics_process(delta):
+	handle_input()
+	velocity.y += gravity * delta   # simple gravity
+	move_and_slide()                # moves using velocity
+	update_movement(delta)
+
+func handle_input():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_speed
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = speed * direction
+	
+func update_movement(delta: float) -> void:
+	pass
